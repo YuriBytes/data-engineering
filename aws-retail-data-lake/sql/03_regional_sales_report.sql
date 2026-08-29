7264947a65181a242539b7d9fb479d9536528f3d
Yuri <eurilaya@gmail.com>
2026-08-30 01:04:07 +0800

    Delete aws-retail-data-lake/sql/03_regional_sales_report.sql

--- aws-retail-data-lake/sql/03_regional_sales_report.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  region,
-  COUNT(*) AS order_count,
-  SUM(quantity) AS units_sold,
-  SUM(revenue) AS total_revenue
-FROM retail_lake.curated_orders
-GROUP BY region
-ORDER BY total_revenue DESC;
